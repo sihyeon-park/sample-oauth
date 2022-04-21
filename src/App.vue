47f9374f642c3c678790fcfdac76302639ca9dfe
sihyeon <sihyeon@himedi.com>
2022-04-21 11:59:20 +0900

    1. accounts.google.com 사용

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div
+      id="g_id_onload"
+      data-client_id=""
+      data-context="signin"
+      data-ux_mode="popup"
+      data-login_uri="http://localhost:8080"
+      data-auto_prompt="false"
+    ></div>
+    <div
+      class="g_id_signin"
+      data-type="icon"
+      data-shape="circle"
+      data-theme="outline"
+      data-text="signin_with"
+      data-size="large"
+    ></div>
   </div>
 </template>
 
+<script>
+/* eslint-disable */
+
+export default {
+  mounted() {
+    google.accounts.id.initialize({
+      client_id:
+        "419276155718-s91buqp4ohlgfitr1e6pig5mjs14p53c.apps.googleusercontent.com",
+      callback: this.handleCredentialResponse,
+    });
+    google.accounts.id.prompt();
+  },
+  methods: {
+    handleCredentialResponse(response) {
+      console.log(response);
+      console.log(this.parseJwt(response.credential));
+    },
+    parseJwt(token) {
+      const base64Url = token.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const jsonPayload = decodeURIComponent(
+        window
+          .atob(base64)
+          .split("")
+          .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+          .join("")
+      );
+      return JSON.parse(jsonPayload);
+    },
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  height: 100vh;
+  @extend .flex-center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+/* eslint-disable */
+import Vue from "vue";
+import App from "./App.vue";
+import { Loading } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.config.productionTip = false;
+Vue.use(Loading);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
